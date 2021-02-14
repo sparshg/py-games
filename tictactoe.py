fbca1de209a4bdcd5b0875ce372ec98656d601f4
sparshg <43041139+sparshg@users.noreply.github.com>
2021-02-14 21:59:52 +0530

    Circle animation added, more fixes (#6)

--- tictactoe.py
+++ tictactoe.py
@@ -9,12 +9,14 @@ import math
 # Hide pygame Hello prompt
 environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
+from pygame.math import Vector2
 
 # Declare some constants and variables
 WIDTH, HEIGHT = (600, 400)
 FPS = 60
 WHITE = "#f1f1f1"
 BLACK = "#101010"
+ORANGE = "#ff6600"
 
 
 class Frame:
@@ -47,6 +49,11 @@ class Frame:
             animation.play()
 
 
+class OX:
+    def __init__(self, _type):
+        self.type = _type
+
+
 class Animate:
 
     LINEAR = lambda x: x
@@ -57,31 +64,81 @@ class Animate:
     def EASE_IO_QUART(x):
         return 8 * pow(x, 4) if x < 0.5 else 1 - pow(-2 * x + 2, 4) / 2
 
-    def __init__(self, dur=1000, fn=EASE_OUT_QUART):
+    def __init__(self, dur=500, color=WHITE, fn=EASE_OUT_QUART):
+        self.color = color
         self.function = fn
         self.dur = dur
         self.start_time = pygame.time.get_ticks()
         self.final_time = self.start_time + dur
-        self.finished = False
         self.type = None
 
-    def line(self, p1, p2):
+    def line(self, p1, p2, width=8):
         self.type = "line"
-        self.p1 = pygame.Vector2(p1)
-        self.p2 = pygame.Vector2(p2)
+        self.finished = False
+        self.width = width
+        self.p1 = Vector2(p1)
+        self.p2 = Vector2(p2)
         self.p = self.p2 - self.p1
         self.length = self.p.magnitude()
         return self
 
-    def play(self):
-        if not self.finished and pygame.time.get_ticks() < self.final_time:
-            fraction = (pygame.time.get_ticks() - self.start_time) / self.dur
-            if self.type == "line":
-                self.p.scale_to_length(self.length * self.function(fraction))
-        else:
-            self.p = self.p2 - self.p1
-            self.finished = True
-        pygame.draw.line(Main.win, WHITE, self.p1, self.p + self.p1, 8)
+    def circle(self, center=[WIDTH / 2, HEIGHT / 2], radius=40, width=8):
+        self.type = "circle"
+        self.finished = False
+        self.width = width
+        self.radius = radius
+        self.center = Vector2(center)
+        self.rect = pygame.Rect(
+            (center - Vector2(radius, radius)), (2 * radius, 2 * radius)
+        )
+        return self
+
+    def cross(self, center=[WIDTH / 2, HEIGHT / 2], length=40, width=10):
+        self.type = "cross"
+        # fmt: off
+        points = [
+            Vector2(-length, 0), Vector2(length, 0),
+            Vector2(0, length), Vector2(0, -length)
+        ]
+        # fmt: on
+        for point in points:
+            point.update(point.rotate(45) + Vector2(center))
+
+        self.sub_animations = [
+            Animate(self.dur + i * 100, self.color, self.function).line(
+                points[i], points[i + 1], width
+            )
+            for i in range(0, len(points), 2)
+        ]
+        return self
+
+    def play(self, skip=False):
+
+        if self.type == "cross":
+            for animation in self.sub_animations:
+                animation.play()
+            return
+
+        if not self.finished:
+            if pygame.time.get_ticks() < self.final_time and not skip:
+                fraction = (pygame.time.get_ticks() - self.start_time) / self.dur
+                if self.type == "line":
+                    self.p.scale_to_length(self.length * self.function(fraction))
+                elif self.type == "circle":
+                    self.r = self.radius - self.width * self.function(fraction)
+            else:
+                if self.type == "line":
+                    self.p = self.p2 - self.p1
+                if self.type == "circle":
+                    self.r = self.radius - self.width
+                self.finished = True
+        if self.type == "line":
+            pygame.draw.line(
+                Main.win, self.color, self.p1, self.p + self.p1, self.width
+            )
+        elif self.type == "circle":
+            pygame.draw.circle(Main.win, self.color, self.center, self.radius)
+            pygame.draw.circle(Main.win, BLACK, self.center, self.r)
 
 
 # The main controller
