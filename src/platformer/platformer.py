0b60081feeb9bdbe2bf5097488c755fd6637f916
sparshg <43041139+sparshg@users.noreply.github.com>
2021-02-23 01:42:16 +0530

    Revert "Platformer Base Created" (#12)

--- src/platformer/platformer.py
+++ src/platformer/platformer.py
@@ -3,52 +3,41 @@
 Github repo can be found here:
 https://github.com/sparshg/pycollab
 """
-
-import pygame as pg
 from os import environ
 
 # Hide pygame Hello prompt
 environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+import pygame
 
 # Declare some constants and variables
 WIDTH, HEIGHT = (600, 400)
 FPS = 60
-BLACK = (0  , 0  , 0  )
-WHITE = (255, 255, 255)
+WHITE = "#F1F1F1"
+BLACK = "#101010"
 
 # The main controller
 class Main:
     def __init__(self):
-        pg.init()
-        pg.display.set_caption("Platformer")
+        pygame.init()
+        pygame.display.set_caption("Platformer")
 
+        self.win = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
-        self.clock = pg.time.Clock()
-        self.win = pg.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
 
     def check_events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 main.running = False
 
     # Update things
     def update(self):
-        player.yVel += physics.gravity
-        player.y += player.yVel
-        if player.colliding():
-            while player.colliding():
-                if player.yVel < 0:
-                    pass
-                elif player.yVel > 0:
-                    pass
-            yVel = 0
+        pass
 
     # Draw things
     def draw(self):
         self.win.fill(BLACK)
-        player.render()
-        platforms.render()
-        pg.display.update()
+        pygame.display.update()
 
     # The main loop
     def loop(self):
@@ -57,43 +46,10 @@ class Main:
             self.update()
             self.draw()
             self.clock.tick(FPS)
-        pg.quit()
-main = Main()
+        pygame.quit()
 
-class Physics:
-    def __init__(self):
-        self.gravity = 2
-        self.jumpHeight = 18
-        self.friction = 2
-        self.acceleration = 4
-
-    def squareCollision(x1, y1, w1, h1, x2, y2, w2, h2):
-        return ((x1 + w1 > x2) and ( x1 < x2 + w2)) and ((y1 + h1 > y2) and ( y1 < y2 + h2))
-physics = Physics()
-
-class Player:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.xVel = 0
-        self.yVel = 0
-
-    def render(self):
-        pg.draw.rect(main.win, WHITE, (self.x, self.y, 50, 50))
-
-    def colliding(self):
-        pass
-player = Player()
-
-class Platforms:
-    def __init__(self):
-        self.platforms = []
-
-    def render(self):
-        for i in range(len(self.platforms)):
-            pg.draw.rect(main.win, WHITE, (self.platforms[i].x, self.platforms[i].y, 75, 75))
-platforms = Platforms()
 
 # Test if the script is directly ran
 if __name__ == "__main__":
+    main = Main()
     main.loop()
