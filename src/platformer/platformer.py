52b65c8d03a4995e449e6615328506e16430e291
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-03-03 18:23:28 +0000

    Update platformer.py

--- src/platformer/platformer.py
+++ src/platformer/platformer.py
@@ -26,23 +26,29 @@ class Main:
         self.clock = pg.time.Clock()
         self.win = pg.display.set_mode((WIDTH, HEIGHT))
 
-        self.player = Player()
-        self.platforms = Platforms()
-
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 main.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_RIGHT: Keys.rtArrow = True
+                else: Keys.rtArrow = False
+                if event.key == pg.K_LEFT: Keys.ltArrow = True
+                else: Keys.ltArrow = False
+                if event.key == pg.K_UP: Keys.upArrow = True
+                else: Keys.upArrow = False
+                if event.key == pg.K_DOWN: Keys.dnArrow = True
+                else: Keys.dnArrow = False
 
     # Update things
     def update(self):
-        self.player.update()
+        Player.update()
 
     # Draw things
     def render(self):
         self.win.fill(BLACK)
-        platforms.render()
-        self.player.render()
+        Platforms.render()
+        Player.render()
         pg.display.update()
 
     # The main loop
@@ -54,15 +60,18 @@ class Main:
             self.clock.tick(FPS)
         pg.quit()
 
+class Keys:
+    rtArrow = False
+    ltArrow = False
+    upArrow = False
+    dnArrow = False
 
 class Physics:
-
-    gravity = 2
+    gravity = 1
     jumpHeight = 18
     friction = 2
     acceleration = 4
 
-    @staticmethod
     def squareCollision(x1, y1, w1, h1, x2, y2, w2, h2):
         return ((x1 + w1 > x2) and (x1 < x2 + w2)) and (
             (y1 + h1 > y2) and (y1 < y2 + h2)
@@ -70,40 +79,45 @@ class Physics:
 
 
 class Player:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.xVel = 0
-        self.yVel = 0
+    x = 0
+    y = 0
+    xVel = 0
+    yVel = 0
 
-    def colliding(self):
+    def colliding():
+        for i in range(len(Platforms.platforms)):
+            if Physics.squareCollision(Player.x, Player.y, 50, 50, Platforms.platforms[i].x, Platforms.platforms[i].y, 50, 50):
+                return True
         return False
 
-    def update(self):
-        self.yVel += Physics.gravity
-        self.y += self.yVel
-        if self.colliding():
-            while self.colliding():
-                if self.yVel < 0:
-                    pass
-                elif self.yVel > 0:
-                    pass
-            self.yVel = 0
+    def update():
+        Player.yVel += Physics.gravity
+        Player.y += Player.yVel
+        if Player.colliding():
+            while Player.colliding():
+                if Player.yVel < 0:
+                    Player.y += 1
+                elif Player.yVel > 0:
+                    Player.y -= 1
+            Player.yVel = 0
+        Player.x += Player.xVel
+        if Keys.rtArrow: Player.xVel += Physics.acceleration
+        if Keys.ltArrow: Player.xVel -= Physics.acceleration
 
-    def render(self):
-        pg.draw.rect(main.win, WHITE, (self.x, self.y, 50, 50))
+    def render():
+        pg.draw.rect(main.win, WHITE, (Player.x, Player.y, 50, 50))
 
+class Platform:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 class Platforms:
-    def __init__(self):
-        self.platforms = []
+    platforms = [Platform(0, 100), Platform(100, 100), Platform(125, 200)]
 
-    def render(self):
-        for i in range(len(self.platforms)):
-            pg.draw.rect(
-                main.win, WHITE, (self.platforms[i].x, self.platforms[i].y, 75, 75)
-            )
-
+    def render():
+        for i in range(len(Platforms.platforms)):
+            pg.draw.rect(main.win, WHITE, (Platforms.platforms[i].x, Platforms.platforms[i].y, 50, 50))
 
 # Test if the script is directly ran
 if __name__ == "__main__":
