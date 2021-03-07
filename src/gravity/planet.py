67435db756c20a85afdf75937478fc956df62b5f
sparshg <sparshgoenka@gmail.com>
2021-03-08 01:45:58 +0530

    change naming

--- src/gravity/planet.py
+++ src/gravity/planet.py
@@ -2,20 +2,20 @@ import pygame as pg
 from constants import *
 
 
-class Body:
+class Planet:
     def __init__(self, mass, radius, pos=(WIDTH / 2, HEIGHT / 2), vel=(0, 0)):
         self.mass = mass
         self.radius = radius
         self.pos = pg.Vector2(pos)
         self.vel = pg.Vector2(vel)
 
-    def calcUpdate(self, bodies):
+    def calcUpdate(self, planets):
         self.field = pg.Vector2(0, 0)
-        for body in bodies:
-            r = body.pos - self.pos
+        for planet in planets:
+            r = planet.pos - self.pos
             mag = r.magnitude()
             if mag != 0:
-                self.field += G * body.mass * r * mag ** -3
+                self.field += G * planet.mass * r * mag ** -3
 
     def applyUpdate(self, dt):
         self.vel += self.field * dt
--- src/gravity/universe.py
+++ src/gravity/universe.py
@@ -1,21 +1,21 @@
 import pygame as pg
-from body import Body
+from planet import Planet
 from constants import *
 
 
 class Universe:
     def __init__(self):
-        self.bodies = [
-            Body(10, 10, (WIDTH / 2 - 100, HEIGHT / 2), (0, 50)),
-            Body(10, 10, (WIDTH / 2 + 100, HEIGHT / 2), (0, -50)),
+        self.planets = [
+            Planet(10, 10, (WIDTH / 2 - 100, HEIGHT / 2), (0, 50)),
+            Planet(10, 10, (WIDTH / 2 + 100, HEIGHT / 2), (0, -50)),
         ]
 
     def update(self, dt):
-        for body in self.bodies:
-            body.calcUpdate(self.bodies)
-        for body in self.bodies:
-            body.applyUpdate(dt)
+        for planet in self.planets:
+            planet.calcUpdate(self.planets)
+        for planet in self.planets:
+            planet.applyUpdate(dt)
 
     def render(self, surf):
-        for body in self.bodies:
-            body.render(surf)
+        for planet in self.planets:
+            planet.render(surf)
