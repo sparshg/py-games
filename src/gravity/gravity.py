07d279dec22fd568e2d46d720e23a14ec0f2fbc2
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-03-07 18:51:42 +0000

    Update gravity.py

--- src/gravity/gravity.py
+++ src/gravity/gravity.py
@@ -5,7 +5,7 @@ https://github.com/sparshg/pycollab
 """
 
 import pygame as pg
-import sys, math
+import sys, math, random
 
 # Declare some constants and variables
 WIDTH, HEIGHT = (1248, 702)
@@ -13,6 +13,7 @@ FPS = 60
 BLACK = (0, 0, 0)
 GREY = (255/2, 255/2, 255/2)
 WHITE = (255, 255, 255)
+YELLOW = (255, 200, 0)
 
 # The main controller
 class Main:
@@ -25,6 +26,7 @@ class Main:
 
         self.planets = Planets()
         self.player = Player()
+        self.goal = Goal(400, 475, 25)
 
     # For key press detection and closing the window properly
     def checkEvents(self):
@@ -41,6 +43,7 @@ class Main:
         self.win.fill(BLACK)
         self.planets.render()
         self.player.render()
+        self.goal.render()
         pg.display.update()
 
     # The main loop
@@ -63,29 +66,100 @@ class Planet:
 
 class Planets:
     def __init__(self):
-        self.planets = [Planet(50, 50, 40)]
+        self.planets = [Planet(50, 150, 5), Planet(225, 300, 20), Planet(400, 75, 50)]
 
     def render(self):
         for i in range(len(self.planets)):
-            pg.draw.circle(main.win, WHITE, (self.planets[i].x+self.planets[i].size, self.planets[i].y+self.planets[i].size), self.planets[i].size)
+            pg.draw.circle(main.win, WHITE, (self.planets[i].x, self.planets[i].y), self.planets[i].size)
 
 
 class Player:
     def __init__(self):
-        self.x = 1
-        self.y = 1
+        self.x = 50
+        self.y = 50
         self.xVel = 0
         self.yVel = 0
 
     def update(self):
+        # Controls
+        keyDown = pg.key.get_pressed()
+        if keyDown[pg.K_RIGHT]:
+            main.player.xVel += Physics.controlPower
+        if keyDown[pg.K_LEFT]:
+            main.player.xVel -= Physics.controlPower
+        if keyDown[pg.K_UP]:
+            main.player.yVel -= Physics.controlPower
+        if keyDown[pg.K_DOWN]:
+            main.player.yVel += Physics.controlPower
+
+        # Gravity
         for i in range(len(main.planets.planets)):
-            self.xVel += math.hypot(self.x - main.planets.planets[i].x, self.y - main.planets.planets[i].y) / 10 * main.planets.planets[i].size
-            self.yVel += math.hypot(self.x - main.planets.planets[i].x, self.y - main.planets.planets[i].y) / 10 * main.planets.planets[i].size
-        self.x += self.xVel * main.dt
-        self.y += self.yVel * main.dt
+            # Direction = atan2(destination y - y, destination x - x)
+            # That finds the direction if you're trying to point somewhere (which is (in this case) "main.planets.planets[i]")
+            dir = math.atan2(main.planets.planets[i].y - self.y, main.planets.planets[i].x - self.x)
+            # Distance = hypot(x1 - x2, y1 - y2)
+            # Finds the dist between 2 points (multiplied by Physics.gravity to adjust the vel)
+            dist = math.hypot(self.x - main.planets.planets[i].x, self.y - main.planets.planets[i].y)
+
+            self.xVel += math.cos(dir) * main.planets.planets[i].size / (dist * Physics.gravity)
+            self.yVel += math.sin(dir) * main.planets.planets[i].size / (dist * Physics.gravity)
+
+            # Bounce if touching planet
+            if dist < main.planets.planets[i].size + 25:
+                self.xVel *= -1
+                self.yVel *= -1
+
+            self.x += self.xVel * main.dt
+            self.y += self.yVel * main.dt
+
+        # Bounce if on edge
+        if self.x - 25 < 0 or self.x + 25 > WIDTH:
+            self.xVel *= -1
+        if self.y - 25 < 0 or self.y + 25 > HEIGHT:
+            self.yVel *= -1
 
     def render(self):
-        pg.draw.circle(main.win, GREY, (self.x+25, self.y+25), 25)
+        pg.draw.circle(main.win, GREY, (self.x, self.y), 25)
+
+
+class Physics:
+    gravity = 0.5
+    # How much planets attract the player (the bigger, the more they do)
+    controlPower = 0.1
+    # How powerful the controls of the player are
+
+
+class Goal:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.particles = []
+
+    def render(self):
+        pg.draw.circle(main.win, YELLOW, (self.x, self.y), self.size)
+        for i in range(len(self.particles)):
+            self.particles[i].render()
+
+
+    def confetti(self):
+        for i in range(math.random() * 20):
+            self.particles.append(Particle(main.goal.x, main.goal.y, random.randint(5, 10), math.random(0, 6.28318531)))
+
+
+class Particle:
+    def __init__(self, x, y, xVel, yVel, dir):
+        self.x = x
+        self.y = y
+        self.xVel = xVel
+        self.yVel = yVel
+        self.dir = dir
+
+    def update(self):
+        pass
+
+    def render(self):
+        pg.draw.circle(main.win, YELLOW, (self.x, self.y), main.goal.size / 4)
 
 
 # Test if the script is directly ran
