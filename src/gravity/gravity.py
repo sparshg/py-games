3ab705fd0941e8c02631ab9f73fc1414e78aaa4f
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-03-07 12:42:41 +0000

    Update gravity.py

--- src/gravity/gravity.py
+++ src/gravity/gravity.py
@@ -5,21 +5,26 @@ https://github.com/sparshg/pycollab
 """
 
 import pygame as pg
-import sys
+import sys, math
 
 # Declare some constants and variables
 WIDTH, HEIGHT = (1248, 702)
 FPS = 60
-BLACK = "#000000"
+BLACK = (0, 0, 0)
+GREY = (255/2, 255/2, 255/2)
+WHITE = (255, 255, 255)
 
 # The main controller
 class Main:
     def __init__(self):
         pg.init()
-        pg.display.set_caption("gravity")
+        pg.display.set_caption("Gravity")
         self.win = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
         self.running = True
+
+        self.planets = Planets()
+        self.player = Player()
 
     # For key press detection and closing the window properly
     def checkEvents(self):
@@ -29,11 +34,13 @@ class Main:
 
     # Update things
     def update(self):
-        pass
+        self.player.update()
 
     # Draw things
     def render(self):
         self.win.fill(BLACK)
+        self.planets.render()
+        self.player.render()
         pg.display.update()
 
     # The main loop
@@ -45,6 +52,40 @@ class Main:
             self.render()
         pg.quit()
         sys.exit()
+
+
+class Planet:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+
+class Planets:
+    def __init__(self):
+        self.planets = [Planet(50, 50, 40)]
+
+    def render(self):
+        for i in range(len(self.planets)):
+            pg.draw.circle(main.win, WHITE, (self.planets[i].x+self.planets[i].size, self.planets[i].y+self.planets[i].size), self.planets[i].size)
+
+
+class Player:
+    def __init__(self):
+        self.x = 1
+        self.y = 1
+        self.xVel = 0
+        self.yVel = 0
+
+    def update(self):
+        for i in range(len(main.planets.planets)):
+            self.xVel += math.hypot(self.x - main.planets.planets[i].x, self.y - main.planets.planets[i].y) / 10 * main.planets.planets[i].size
+            self.yVel += math.hypot(self.x - main.planets.planets[i].x, self.y - main.planets.planets[i].y) / 10 * main.planets.planets[i].size
+        self.x += self.xVel * main.dt
+        self.y += self.yVel * main.dt
+
+    def render(self):
+        pg.draw.circle(main.win, GREY, (self.x+25, self.y+25), 25)
 
 
 # Test if the script is directly ran
